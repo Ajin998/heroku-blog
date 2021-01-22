49dc2881492c4a9d7e77d9589c47d17b89fa4b10
Ajin Varghese <ajin.varghese@contentstack.com>
2021-01-22 13:33:52 +0530

    Solved CORS issue

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const Blogs = require("./routes/blogsRoute.js");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config({ path: "./.env" });
 
 app.use(express.json());
+app.use(cors());
 app.use("/", Blogs);
 app.listen(process.env.PORT, console.log("Server up and Running on PORT 3000"));
